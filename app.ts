a9423f7629717e158b164c48b6251d3692729ac3
ergouwang <330035385@qq.com>
2020-06-21 18:49:32 +0800

    使用koa执行特性统计打印所用时长

--- app.ts
+++ app.ts
@@ -1,19 +1,24 @@
 import * as Koa from 'koa'
 const  app = new Koa()
-/**
- * koa 的执行过程是一个 U 型的模型
- * 所以下面代码的执行顺序是：
- * 先执行第10行，再执行第16行，最后回过来再执行第12行（见页面输出）
- */
+
+//记录hello从开始到写完所用时长
 
 app.use(async (ctx,next)=>{
-    ctx.body = 'hello'
-    await next()
-    ctx.body += ' callback'
+    await next();
+    const time = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.url}-${time}`);
 })
 
+app.use(async (ctx,next)=>{
+    const start = Date.now()
+    await next()
+    const time = Date.now() - start;
+    ctx.set('X-Response-Time',`${time}ms`);
+})
+
+
 app.use(async (ctx)=>{
-    ctx.body += ' koa'
+    ctx.body = 'hello world'
 })
 
 app.listen(3000)
